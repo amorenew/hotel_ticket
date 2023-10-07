dc27cc5202c7f4e5f9d08a6b9f56fd0b243cd1d3
Amr Eniou <amorenew@gmail.com>
2023-10-08 00:34:17 +0300

    fix data for android

--- android/app/src/main/kotlin/com/example/hotel_ticket/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/hotel_ticket/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.hotel_ticket
 
+import android.widget.TextView
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.squareup.picasso.Picasso
+import de.hdodenhof.circleimageview.CircleImageView
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 
@@ -18,16 +21,33 @@ class MainActivity : FlutterActivity() {
 
     class TicketApi(private val context: FlutterActivity) : TicketHostApi {
         override fun sendTicketData(ticketData: TicketData, callback: (Result<Boolean>) -> Unit) {
-            showBottomSheetDialog(context);
+            showBottomSheetDialog(context,ticketData);
         }
 
     }
 
     companion object {
-        private fun showBottomSheetDialog(context:FlutterActivity ) {
+        private fun showBottomSheetDialog(context: FlutterActivity, ticketData: TicketData) {
             val bottomSheetDialog = BottomSheetDialog(context)
             bottomSheetDialog.setContentView(R.layout.bottom_sheet_dialog)
-            //val copy = bottomSheetDialog.findViewById<LinearLayout>(R.id.copyLinearLayout)
+
+            val tvName = bottomSheetDialog.findViewById<TextView>(R.id.tvName)
+            val tvTicketNumber = bottomSheetDialog.findViewById<TextView>(R.id.tvTicketNumber)
+            val tvType = bottomSheetDialog.findViewById<TextView>(R.id.tvType)
+            val tvSeat = bottomSheetDialog.findViewById<TextView>(R.id.tvSeat)
+            val profileImage = bottomSheetDialog.findViewById<CircleImageView>(R.id.profileImage)
+
+            tvName?.text = ticketData.name;
+            tvTicketNumber?.text = ticketData.ticketNumber;
+            tvType?.text = ticketData.type;
+            tvSeat?.text = ticketData.seat;
+
+            Picasso.get()
+                .load(ticketData.imageUrl)
+                //.resize(35, 35)
+                //.centerCrop()
+                .into(profileImage)
+
             bottomSheetDialog.show()
         }
 
