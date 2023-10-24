93fb22500361fcf41179b05955c95f5c89ad742b
Amr Eniou <amorenew@gmail.com>
2023-10-24 21:48:52 +0300

    add local json request

--- lib/data/providers/user_event/event_provider.dart
+++ lib/data/providers/user_event/event_provider.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/services.dart';
 import 'package:hotel_ticket/data/models/events_response.dart';
 import 'package:hotel_ticket/data/providers/user_event/base_event_provider.dart';
 import 'package:hotel_ticket/utils/http_service.dart';
@@ -7,7 +8,13 @@ import 'package:hotel_ticket/constant/urls.dart';
 
 class EventProvider extends BaseEventProvider {
   @override
-  Future<EventsResponse?> getEvents() async {
+  Future<EventsResponse?> getEvents({bool useAssets = true}) async {
+    if (useAssets) {
+      final localResponse = await loadEventsFromAssets();
+      var data = json.decode(localResponse);
+      return EventsResponse.fromJson(data);
+    }
+
     var response = await apiCall(url: urlListEvents);
     if (response?.statusCode == 200) {
       var data = json.decode(response.toString());
@@ -16,4 +23,7 @@ class EventProvider extends BaseEventProvider {
       return EventsResponse(error: true);
     }
   }
+
+  Future<String> loadEventsFromAssets() async =>
+      await rootBundle.loadString('assets/response.json');
 }
